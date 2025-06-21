048e8789df7384e9a6bf94a03df5724dfbd7d2d9
willian017 <willianyuji100@gmail.com>
2025-06-21 13:29:00 -0300

    feat: add initial structure and files for ToDoList application

--- Index.cpp
+++ Index.cpp
@@ -1,3 +1,11 @@
 #include <conio2.h>
+#include <string.h>
+#include <ctype.h>
 #include <stdio.h>
+#include "TAD.h"
+#include "Menu.h"
 
+
+int main()
+{
+}
---
+++ Menu.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <conio2.h>
+
+#define CIMA 72
+#define BAIXO 80
+#define ENTER 13
