0661831023852d5a123297109e253b6a6c73f2bd
willian017 <willianyuji100@gmail.com>
2025-06-21 19:02:23 -0300

    feat: implement Tarefa structure and Read function for task management

--- TAD.h
+++ TAD.h
@@ -1,3 +1,9 @@
+struct Tarefa
+{
+    int status, prioridade;
+    char desc[100], data[10];
+};
+
 struct Tarefas
 {
     int local;
@@ -7,5 +13,47 @@ struct Tarefas
 struct TarefasC
 {
     int local;
-    Tarefas *prox, *ant;
+    TarefasC *prox, *ant;
 };
+
+void Read(Tarefas *Inicio, TarefasC *InicioC)
+{
+    int pos;
+    Tarefa AuxT;
+
+    Inicio -> prox = Inicio -> ant = NULL;
+    InicioC -> prox = InicioC -> ant = NULL;
+
+    FILE *Ptr = fopen("rb+","Tarefas.txt");
+
+    if(!feof(Ptr))
+    {
+        pos = fseek(Ptr,,Aux);
+        Tarefas *Aux;
+        TarefasC *AuxC;
+
+        do
+        {
+            if(AuxT.status == 0)
+            {
+                if(Inicio -> prox == NULL)
+                {
+                    Aux -> local = pos;
+                    Inicio -> prox = Aux;
+                    Aux -> ant = Aux -> prox = NULL;
+                }
+            }
+            else
+            {
+                if(InicioC -> prox == NULL)
+                {
+                    Aux -> local = pos;
+                    InicioC -> prox = Aux;
+                    Aux -> ant = Aux -> prox = NULL;
+                }
+            }
+
+            fread(Ptr,,Aux);
+        }while(!feof(Ptr));
+    }
+}
