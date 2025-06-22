387b0ad708c6c4bda21a34d8792498364416fb1d
willian017 <willianyuji100@gmail.com>
2025-06-22 11:29:52 -0300

    feat: enhance Tarefas and TarefasC structures with prioridade field and improve Read function logic

--- TAD.h
+++ TAD.h
@@ -6,13 +6,13 @@ struct Tarefa
 
 struct Tarefas
 {
-    int local;
+    int local, prioridade;
     Tarefas *prox, *ant;
 };
 
 struct TarefasC
 {
-    int local;
+    int local, prioridade;
     TarefasC *prox, *ant;
 };
 
@@ -29,28 +29,56 @@ void Read(Tarefas *Inicio, TarefasC *InicioC)
     if(!feof(Ptr))
     {
         pos = fseek(Ptr,,Aux);
-        Tarefas *Aux;
-        TarefasC *AuxC;
-
+        
         do
         {
             if(AuxT.status == 0)
             {
+                Tarefas *Aux = new Tarefas;
+
+                Aux -> local = pos;
+                Aux -> prioridade = AuxT.prioridade;
+                Aux -> ant = Aux -> prox = NULL;
+ 
                 if(Inicio -> prox == NULL)
-                {
-                    Aux -> local = pos;
                     Inicio -> prox = Aux;
-                    Aux -> ant = Aux -> prox = NULL;
+                else if(Inicio -> prox -> prioridade < AuxT.prioridade)
+                {
+                    Aux -> prox = Inicio -> prox;
+                    Inicio -> prox -> ant = Aux;
+                    Inicio -> prox = Aux;
+                }
+                else
+                {
+                    Aux -> prox = Inicio -> prox;
+
+                    while(Aux -> prox -> prox != NULL && Aux -> prox -> prioridade >= AuxT.prioridade)
+                        Aux -> prox = Aux -> prox -> prox;
+
+                    if(Aux -> prox -> prioridade < AuxT.prioridade)
+                    {
+                        Aux -> ant = Aux -> prox -> ant;
+                        Aux -> prox -> ant = Aux -> prox -> ant -> prox = Aux;
+                    }
+                    else
+                    {
+                        Aux -> prox -> prox = Aux;
+                        Aux -> ant = Aux -> prox;
+                        Aux -> prox = NULL;
+                    }
                 }
             }
             else
             {
+                TarefasC *AuxC = new TarefasC;
+
+                AuxC -> local = pos;
+                AuxC -> prioridade = AuxT.prioridade;
+                AuxC -> ant = AuxC -> prox = NULL;
+
+
                 if(InicioC -> prox == NULL)
-                {
-                    Aux -> local = pos;
-                    InicioC -> prox = Aux;
-                    Aux -> ant = Aux -> prox = NULL;
-                }
+                    InicioC -> prox = AuxC;
             }
 
             fread(Ptr,,Aux);
